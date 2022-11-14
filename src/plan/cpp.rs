42b91a3ecf730167f325e7dec163e3212e259f0a
Tristan Ravitch <tristan@ravit.ch>
2022-11-14 00:18:00 -0800

    Make the C++ argument parser a bit more robust
    It isn't 100% solid yet. It needs to handle qualifiers.

--- src/plan/cpp.rs
+++ src/plan/cpp.rs
@@ -62,14 +62,37 @@ fn callable_name_node_to_string(n : &Node, src : & [u8]) -> String {
 }
 
 fn parameter_node_to_argument<'a>(n : &'a Node, src : &'a [u8]) -> FormalArgument {
-    // FIXME: Rearrange the types to enable clean error handling (i.e., add a Result to the return)
-    let ident_node = n.child(1).unwrap();
-    let ident = ident_node.utf8_text(src).unwrap();
+    if n.child_count() == 1 {
+        // In this case, it is just an unused argument with a type and no name
+        let type_node = n.child(0).unwrap();
+        let ty = type_node.utf8_text(src).unwrap();
+        FormalArgument {
+            name: "".into(), // FIXME: Change this type
+            declared_type: Some(ty.into())
+        }
+    } else if n.child_count() == 2 {
+        // FIXME: Rearrange the types to enable clean error handling (i.e., add a Result to the return)
+        let ident_node = n.child(1).unwrap();
+        let ident = ident_node.utf8_text(src).unwrap();
 
-    let type_node = n.child(0).unwrap();
-    let ty = type_node.utf8_text(src).unwrap();
-    FormalArgument {
-        name: ident.into(),
-        declared_type: Some(ty.into())
+        let type_node = n.child(0).unwrap();
+        let ty = type_node.utf8_text(src).unwrap();
+        FormalArgument {
+            name: ident.into(),
+            declared_type: Some(ty.into())
+        }
+    } else if n.child_count() == 3 {
+        // FIXME: Capture qualifiers
+        let ident_node = n.child(2).unwrap();
+        let ident = ident_node.utf8_text(src).unwrap();
+
+        let type_node = n.child(1).unwrap();
+        let ty = type_node.utf8_text(src).unwrap();
+        FormalArgument {
+            name: ident.into(),
+            declared_type: Some(ty.into())
+        }
+    } else {
+        panic!("Unknown argument structure with {} children", n.child_count());
     }
 }
