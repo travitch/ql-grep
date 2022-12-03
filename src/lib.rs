b9007eaf3752120a4efcc3c63dc602ed62605849
Tristan Ravitch <tristan@ravit.ch>
2022-12-02 19:57:04 -0800

    Clean up the library exports
    Now only export what is actually required, simplifying the imports for client
    code.  More can be exported over time - this is just a draft of what the
    interface should be.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
-pub mod evaluate;
-pub mod library;
-pub mod compile;
-pub mod query;
-pub mod source_file;
+mod compile;
+mod evaluate;
+mod library;
+mod query;
+mod source_file;
+
+pub use crate::compile::compile_query;
+pub use crate::evaluate::{evaluate_plan, QueryResult};
+pub use crate::library::LIBRARY_DATA;
+pub use crate::query::ir::{Syntax, Typed};
+pub use crate::query::typecheck::typecheck_query;
+pub use crate::query::{parse_query, Query};
+pub use crate::source_file::SourceFile;
