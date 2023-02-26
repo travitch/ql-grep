2076ab985923e790a6d4c91ce1a91a0ac9ed2b99
Tristan Ravitch <tristan@ravit.ch>
2023-02-25 20:38:11 -0800

    Add a test for the include feature
    This tests:
    
    - Include queries
    - Negation
    - Relational rewriting
    
    The relational parts of this query also triggered a bug in the typchecker fixed
    in the previous commit.

---
+++ tests/codebases/feature/0016-c-find-use-without-include/ok.c
@@ -0,0 +1,11 @@
+#include <string.h>
+
+extern const char * s;
+
+int f1(const char* foo) {
+  return strcmp(foo, s);
+}
+
+int f2(const char* foo, const char* bar) {
+  return strcmp(bar, s) >= 0;
+}
